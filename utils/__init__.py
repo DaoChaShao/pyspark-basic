5d6c5a839f58b4d0b166fe595b12b0b81f1f111b
DaoChaShao <27882272@qq.com>
2026-08-20 02:40:06 +1200

    feat(utils): initialize PySpark utility toolkit
    - Add main __init__.py file for PySpark utilities package
    - Configure Spark engine startup functionality
    - Include helper modules for CSV operations and fake data generation
    - Set up proper module imports and exports
    - Initialize package metadata with author and version information
    - Add comprehensive module documentation and description

---
+++ utils/__init__.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3.12
+# -*- Coding: UTF-8 -*-
+# @Time     :   2026/8/19 22:49
+# @Author   :   Shawn
+# @Version  :   Version 0.1.0
+# @File     :   __init__.py.py
+# @Desc     :   
+
+
+"""
+
+****************************************************************
+PySpark Utility Toolkit
+----------------------------------------------------------------
+This module provides utility functions for configuring and
+initialising PySpark applications, including Spark engine
+startup and related helper functionality.
+****************************************************************
+"""
+
+__author__ = "Shawn Yu"
+__version__ = "0.1.0"
+
+from .PS import (SparkEngineStarter,
+                 fake_data,
+                 spark_csv)
+
+__all__ = [
+    "SparkEngineStarter",
+    "fake_data",
+    "spark_csv"
+]
